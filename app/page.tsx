f4a526c8dc8557ce7bd77aeb119025e722c97595
Kayson <kcphan1004@gmail.com>
2026-07-11 23:41:47 +0800

    Update homepage sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+// CardZero homepage sections
 import Hero from "../components/Hero";
 import FeatureSection from "../components/FeatureSection";
 import FeaturedDecks from "../components/FeaturedDecks";
